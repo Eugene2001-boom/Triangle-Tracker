7bc6b20ac2fd5b749cac132d854fe9b2a9045848
Eugene2001-boom <leekiplee2001@gmail.com>
2020-07-30 12:24:25 +0300

    add initial page structure

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Function to validate only numbers. No special characters or letters
+
+function isNumberKey(evt) {
+  var charCode = (evt.which) ? evt.which : event.keyCode
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
+}
