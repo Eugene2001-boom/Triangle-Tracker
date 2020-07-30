8051396b4f5feb36d0fe48841d94b4c30c6f5aff
Eugene2001-boom <leekiplee2001@gmail.com>
2020-07-30 12:38:47 +0300

    add initial page structure

--- js/script.js
+++ js/script.js
@@ -24,16 +24,16 @@ function getValues() {
   
     //Conditional loops to check type of triangle based on decision matrix
 
-  if (sideA === sideB && sideB === sideC && sideA === sideC) {
-    console.log('equilateral');
-    alert("This is an equilateral Triangle");
-  } else if ((side1 == null || side1 === "" || side1 === 0) || (side2 == null || side2 === "") || (side3 == null || side3 === "")) {
-    alert("All sides required");
-  } else if (sideA + sideB > sideC && sideA != sideB && sideB != sideC || sideA + sideC > sideB && sideA != sideB && sideB != sideC || sideC + sideB > sideA && sideA != sideB && sideB != sideC) {
-    alert("This is a Scalene Triangle");
-  } else if (sideA === sideB && sideB != sideC || sideA === sideC && sideC != sideB || sideB === sideC && sideC != sideA) {
-    console.log('isosceles');
-    alert("This is an Isosceles Triangle")
+    if (sideA === sideB && sideB === sideC && sideA === sideC) {
+      console.log('equilateral');
+      alert("This is an equilateral Triangle");
+    } else if (sideA === sideB && sideB != sideC || sideA === sideC && sideC != sideB || sideB === sideC && sideC != sideA) {
+      console.log('isosceles');
+      alert("This is an Isosceles Triangle")
+    } else if (sideA + sideB > sideC && sideA != sideB && sideB != sideC || sideA + sideC > sideB && sideA != sideB && sideB != sideC || sideC + sideB > sideA && sideA != sideB && sideB != sideC) {
+      alert("This is a Scalene Triangle");
+    } else if ((side1 == null || side1 === "" || side1 === 0) || (side2 == null || side2 === "") || (side3 == null || side3 === "")) {
+      alert("All sides required");
   } else {
     return 0;
   }
